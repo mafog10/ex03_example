ab3d69a1864973b4ecd16310ba37965cc6ef0ba8
mafog10 <maxim.fournier-giguere.1@ulaval.ca>
2013-09-19 21:37:07 -0400

    initial version

---
+++ README.md
@@ -0,0 +1,9 @@
+##exercice_3_sio_6014
+
+**Categories**: education
+
+**Description**: exercice 3 - utilisation de la librairie
+
+**By**: Maxim Fournier-Giguère
+
+**For**: SIO-6014
---
+++ bin/ex03_example.dart
@@ -0,0 +1,8 @@
+//import '../../ex03_library/lib/ex03_library.dart';  //pour tests en local
+
+import 'package:ex03_library/ex03_library.dart';
+
+void main() {
+  ex2_1(10.0, 20.0);
+  ex2_2(100.0, 9.58);
+}
