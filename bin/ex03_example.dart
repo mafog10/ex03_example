ee9cb12644867abc4785c685726858741b6b4814
mafog10 <maxim.fournier-giguere.1@ulaval.ca>
2013-09-20 20:18:10 -0400

    update label

--- bin/ex03_example.dart
+++ bin/ex03_example.dart
@@ -1,8 +1,8 @@
-//import '../../ex03_library/lib/ex03_library.dart';  //pour tests en local
-
 import 'package:ex03_library/ex03_library.dart';
 
 void main() {
+  print("Exemple d'utilisation de la librairie ex03_librairie");
+  print('');
   ex2_1(10.0, 20.0);
   ex2_2(100.0, 9.58);
 }
